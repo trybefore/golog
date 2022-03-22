fb91b785e445b6bf2d8448e00f3f3985714ae7ec
Daniel Boman <daniel.boman@pm.me>
2022-03-22 14:28:39 +0000

    added metrics, traces and logging
    Signed-off-by: Daniel Boman <daniel.boman@pm.me>

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -2,20 +2,39 @@ package server
 
 import (
 	"context"
+	"flag"
 	"io/ioutil"
 	"net"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"github.com/trybefore/golog/api"
 	"github.com/trybefore/golog/internal/auth"
 	"github.com/trybefore/golog/internal/config"
 	"github.com/trybefore/golog/internal/log"
+	"go.opencensus.io/examples/exporter"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/status"
 )
+
+var debug = flag.Bool("debug", false, "Enable observability for debugging")
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	if *debug {
+		logger, err := zap.NewDevelopment()
+		if err != nil {
+			panic(err)
+		}
+		zap.ReplaceGlobals(logger)
+	}
+	os.Exit(m.Run())
+}
 
 func TestServer(t *testing.T) {
 	for scenario, fn := range map[string]func(t *testing.T, rootClient api.LogClient, unauthClient api.LogClient, config *Config){
@@ -85,6 +104,27 @@ func setupTest(t *testing.T, fn func(*Config)) (rootClient api.LogClient, unauth
 	require.NoError(t, err)
 
 	authorizer := auth.New(config.ACLModelFile, config.ACLPolicyFile)
+	var telemetryExporter *exporter.LogExporter
+
+	if *debug {
+		metricsLogFile, err := ioutil.TempFile("", "metrics-*.log")
+		require.NoError(t, err)
+		t.Logf("metrics log file: %s", metricsLogFile.Name())
+		traceLogFile, err := ioutil.TempFile("", "traces-*.log")
+		require.NoError(t, err)
+		t.Logf("trace log file: %s", traceLogFile.Name())
+
+		telemetryExporter, err = exporter.NewLogExporter(exporter.Options{
+			MetricsLogFile:    metricsLogFile.Name(),
+			TracesLogFile:     traceLogFile.Name(),
+			ReportingInterval: time.Second,
+		})
+		require.NoError(t, err)
+
+		err = telemetryExporter.Start()
+		require.NoError(t, err)
+	}
+
 	cfg = &Config{
 		CommitLog:  clog,
 		Authorizer: authorizer,
@@ -106,6 +146,11 @@ func setupTest(t *testing.T, fn func(*Config)) (rootClient api.LogClient, unauth
 		rootConn.Close()
 		unauthConn.Close()
 		l.Close()
+		if telemetryExporter != nil {
+			time.Sleep(time.Second * 2)
+			telemetryExporter.Stop()
+			telemetryExporter.Close()
+		}
 	}
 
 }
